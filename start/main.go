4fb618b09dcb7fe3ee0e56432b68f9ce13103369
Made Indra <made.indra@protonmail.com>
2022-11-18 17:40:35 +0700

    add workflow starter

---
+++ start/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	app "github.com/madeindra/temporal-hello-world"
+	"go.temporal.io/sdk/client"
+)
+
+func main() {
+	// create client
+	c, err := client.Dial(client.Options{})
+	if err != nil {
+		log.Fatalln("unable to create Temporal client", err)
+	}
+	defer c.Close()
+
+	// create workflow options
+	options := client.StartWorkflowOptions{
+		ID:        "greeting-workflow",
+		TaskQueue: app.GreetingTaskQueue,
+	}
+
+	// start workflow
+	name := "World"
+	wf, err := c.ExecuteWorkflow(context.Background(), options, app.GreetingWorkflow, name)
+	if err != nil {
+		log.Fatalln("unable to complete Workflow", err)
+	}
+
+	// get results
+	var greeting string
+	err = wf.Get(context.Background(), &greeting)
+	if err != nil {
+		log.Fatalln("unable to get Workflow result", err)
+	}
+
+	printResult(greeting, wf.GetID(), wf.GetRunID())
+}
+
+func printResult(greeting string, workflowID string, runID string) {
+	fmt.Printf("\n WorkflowID: %s RunID: %s \n", workflowID, runID)
+	fmt.Printf("\n %s \n\n", greeting)
+}
